35d7c7369cda892f3703bf9cc168e6af417532ac
pablo.orellana <17915690+iCopyPasta@users.noreply.github.com>
2022-10-18 19:58:08 -0400

    add: ch60-63, assignment

--- react_the_complete_guide/udemy_basic_react_project/src/components/Expenses/Expenses.js
+++ react_the_complete_guide/udemy_basic_react_project/src/components/Expenses/Expenses.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+
 import "./Expenses.css";
+
+// listen to changes on dropdown
+// when user picks a new year
+// should listen to that event and picked year/value
+// is forwarded to xpenses component
+// add expense filter
+// from expensefilter, store returned valued in a state
+// nothing needs to be updated
+
 
 // cp the JSX for now
 const Expenses = (props) => {
+
+    const [enteredFilterYear, setEnteredFilterYear] = useState('2020');
+
+    const handleYearPicked = (yearPicked) => {
+        console.log(yearPicked);
+        setEnteredFilterYear(yearPicked);
+    }
+
+    // Expenses controls Expenses Filter
+    // Presentational/Stateless/Dumb
+    // some components manage state - Expenses
+    // some just present data       - ExpenseItem
     return (
         <Card className="expenses">
+            <ExpenseFilter selected={enteredFilterYear} onYearPicked={handleYearPicked}></ExpenseFilter>
             <ExpenseItem
                 title={props.expenses[0].title}
                 amount={props.expenses[0].amount}
