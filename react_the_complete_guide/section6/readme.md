8b8ff2b697ca551790419f055b2e189fc50c5ccb
pablo.orellana <17915690+iCopyPasta@users.noreply.github.com>
2022-10-24 20:23:30 -0400

    ch80-82, css modules in react2

--- react_the_complete_guide/section6/readme.md
+++ react_the_complete_guide/section6/readme.md
@@ -32,7 +32,7 @@ f'{variable}'
 
 where you can continually add new ```${}```
 
-## Scoping Styling to Styled *Components*
+### Scoping Styling to Styled *Components*
 
 * "Styled Components" Package
   * Styles are attached to the components only
@@ -57,3 +57,57 @@ where you can continually add new ```${}```
   ```html
     <button type="submit" class="sc-AxjAm hHfnXX">
   ```
+
+* Can write all the 'inputs' via the following manner:
+```javacript
+const Button = styled.button`
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+
+  @media(min-width: 768px) {
+    width: auto;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }`;
+
+```
+* props are propagated with the appropriate calls
+(auto-magic?)
+
+## Using CSS Modules
+
+* requires a few changes
+  * react 2.0+
+  * all CSS files are named via the syntax/convention:
+    * ```ComponentName}.module.css```
+    * ```Button.module.css
+  * use the "styles" import 
+    ```javascript
+    import styles from "./Button.module.css";
+    ```
+  * use the prop object reference to utilize a 'signal'
+  ```html
+      <button type={props.type} className={styles.button} onClick={props.onClick}>
+  ```
+    * returns a unique hash
+    * 
+    ```html
+    <button type="submit" class="Button_button_2lgkF"...> 
+    ```
+
+  * access those classes as properties on the imported 'styles' objects  
+Citation: [https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/](https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/)
--- react_the_complete_guide/u_stlying_prac/src/components/UI/Button/Button.js
+++ react_the_complete_guide/u_stlying_prac/src/components/UI/Button/Button.js
@@ -1,41 +1,48 @@
 import React from 'react';
-import styled from 'styled-components';
 
+// for CSS Modules
+// signal to underlying 'compilation'
+import styles from "./Button.module.css";
+
+// import styled from 'styled-components';
 // tagged template literal
 // methods for all HTML elements
 // returns a style w/ the input given
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
 
-  @media(min-width: 768px) {
-    width: auto;
-  }
+//   @media(min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }`;
+// data from props is handled and set correctly from 'above'
 
-  // data from props is handled and set correctly from 'above'
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+
+// standard JSX
+// but will apply in a special way
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 
 
